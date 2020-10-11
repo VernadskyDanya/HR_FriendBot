aae3b25afc4de6ce869202ff54c855b7d20772ab
Vernadsky Danya <d.vernadsky@yandex.ru>
2020-10-11 12:51:08 +0300

    Was added delete messages to avoid mess in chat

--- functions.py
+++ functions.py
@@ -1,31 +1,42 @@
 """Модуль, где хранятся кнопки и рекакции на них"""
 
 #кнопка для возврата к основным вопросам
-def Menu_button(types):
-    button = types.InlineKeyboardButton(text="❔ Задать новый вопрос", callback_data="Start")
+def menu_button(types):
+    button = types.InlineKeyboardButton(text="❔ Задать новый вопрос", callback_data="start")
     markup = types.InlineKeyboardMarkup()
     markup.add(button)
     return markup
 
-def Coffee(message_chat_id, bot, types):
+
+def coffee(message_chat_id, bot, types):
     button1 = types.InlineKeyboardButton(text="Бот Hot Coffee ☕ ",
                                          url="https://t.me/GPN_S_coffee_bot")
     markup = types.InlineKeyboardMarkup()
     markup.add(button1)
-    bot.send_message(message_chat_id, "Для этого создан другой чат бот", reply_markup=markup)
+    bot.send_message(message_chat_id, "Для этого создан другой чат бот", reply_markup= menu_button(types))
 
-def Pers_adm(message_chat_id, bot, types):
+
+def pers_adm(message_chat_id, bot, types):
     button1 = types.InlineKeyboardButton(text="Я оформлен в КЦ", callback_data="KS")
     button2 = types.InlineKeyboardButton(text="Я оформлен в ГПН-С", callback_data="GPN-S")
     markup = types.InlineKeyboardMarkup()
     markup.add(button1, button2)
-    bot.send_message(message_chat_id, 'Выбери отдел:', reply_markup=markup)
+    bot_message = bot.send_message(message_chat_id, 'Выбери отдел:', reply_markup= markup)
+    return bot_message
 
-def KS(message_chat_id, bot, types):
-    button1 = types.InlineKeyboardButton(text="1. ", callback_data="Perenos")
-    button2 = types.InlineKeyboardButton(text="2. ", callback_data="Spravka")
-    button3 = types.InlineKeyboardButton(text="3. ", callback_data="Bolnichnyi")
-    button4 = types.InlineKeyboardButton(text="4. ", callback_data="Komandirovka")
+
+
+def KSorGPN_S(message_chat_id, bot, types, KSorGPN_Sbool):
+    if not KSorGPN_Sbool:
+        button1 = types.InlineKeyboardButton(text="1. ", callback_data="perenosKS")
+        button2 = types.InlineKeyboardButton(text="2. ", callback_data="spravkaKS")
+        button3 = types.InlineKeyboardButton(text="3. ", callback_data="bolnichnyiKS")
+        button4 = types.InlineKeyboardButton(text="4. ", callback_data="komandirovkaKS")
+    else:
+        button1 = types.InlineKeyboardButton(text="1. ", callback_data="perenosGPN_S")
+        button2 = types.InlineKeyboardButton(text="2. ", callback_data="spravkaGPN_S")
+        button3 = types.InlineKeyboardButton(text="3. ", callback_data="bolnichnyiGPN_S")
+        button4 = types.InlineKeyboardButton(text="4. ", callback_data="komandirovkaGPN_S")
     markup = types.InlineKeyboardMarkup()
     markup.add(button1, button2, button3, button4)
     bot.send_message(message_chat_id, "1. Как перенести/отменить отпуск?\n"
@@ -33,7 +44,8 @@ def KS(message_chat_id, bot, types):
                                       "3. Как передать больничный в кадры?\n"
                                       "4. Как оформить командировку?\n", reply_markup=markup)
 
-def Study(message_chat_id,bot,types):
+
+def study(message_chat_id,bot,types):
     button1 = types.InlineKeyboardButton(text="1", callback_data="1")
     button2 = types.InlineKeyboardButton(text="2", callback_data="2")
     button3 = types.InlineKeyboardButton(text="3", callback_data="3")
@@ -45,26 +57,56 @@ def Study(message_chat_id,bot,types):
                                       '3. Хочу учить других!\n'
                                       '4. Как понять, чему мне поучиться?\n', reply_markup=markup)
 
-def Perenos(message_chat_id,bot,types):
+
+def perenosKS(message_chat_id, bot, types):
     bot.send_message(message_chat_id, 'По согласованию с руководителем отпуск можно перенести или отменить в '
                                       'личном кабинете SAP:\n'
                                       '<b>Портал - Услуги и сервисы - Корпоративные сервисы '
                                       'самообслуживания - SAP портал - Управление отпусками - Перенос отпуска</b>\n'
                                       'Если у вас возникли сложности, вы всегда можете обраться по адресу '
                                       '<b>"Фронт-офис ЦКР Почтамтская"</b>', parse_mode = 'HTML',
-                                      reply_markup= Menu_button(types))
-def Spravka(message_chat_id,bot,types):
+                                      reply_markup= menu_button(types))
+def spravkaKS(message_chat_id, bot, types):
     bot.send_message(message_chat_id, 'Спавки можно заказать в личном кабинете SAP: <b>Портал - Услуги и сервисы - '
                                       'Корпоративные сервисы самообслуживания - SAP портал - Заказ справок</b>\n'
                                       'Также справку можно заказать по внтреннему номеру <b>8181</b>,'
                                       ' или по внешнему - <b>8 800 350 8181</b>', parse_mode='HTML',
-                                      reply_markup= Menu_button(types))
-def Bolnichnyi(message_chat_id,bot,types):
+                                      reply_markup= menu_button(types))
+def bolnichnyiKS(message_chat_id, bot, types):
     bot.send_message(message_chat_id, 'Через ячейку на 1-м этаже "Исходящая документация" - на '
                                       'Почтамтскую 3-5, во Фронт-офис ЦКР', parse_mode='HTML',
-                                      reply_markup= Menu_button(types))
-def Komandirovka(message_chat_id,bot,types):
+                                      reply_markup= menu_button(types))
+def komandirovkaKS(message_chat_id, bot, types):
     bot.send_message(message_chat_id, 'Командировку можно оформить в личном кабинете SAP Портала: '
                                       '<b>Услуги и сервисы - Корпоративные сервисы самообслуживания - '
                                       'SAP портал - Командировки</b>', parse_mode='HTML',
-                                      reply_markup= Menu_button(types))
+                                      reply_markup= menu_button(types))
+
+"""
+    Теперь для "У меня вопрос по кадровому администрированию" -> "Я оформлен в ГПН-С" 
+"""
+
+def perenosGPN_S(message_chat_id, bot, types):
+    bot.send_message(message_chat_id, 'Вам необходимо <b>согласовать</b> перенос/отмену отпуска по почте с '
+                                      '<b>линейным и функциональным руководителями</b>, и направить данное '
+                                      'согласование кадровым администраторам ООО "Газпромнеть Снабжения"',
+                                      parse_mode = 'HTML', reply_markup= menu_button(types))
+
+
+def spravkaGPN_S(message_chat_id, bot, types):
+    bot.send_message(message_chat_id, 'Вам необходимо направить запрос на справку кадровым '
+                                      'администраторам ООО "Газпромнеть Снабжения"', parse_mode='HTML',
+                                      reply_markup= menu_button(types))
+
+
+def bolnichnyiGPN_S(message_chat_id, bot, types):
+    bot.send_message(message_chat_id, 'Через ячейку на 1-м этаже "Исходящая документация" - на Малую Морскую, '
+                                      '23 с указанием имени адресата', parse_mode='HTML',
+                                      reply_markup= menu_button(types))
+
+
+def komandirovkaGPN_S(message_chat_id, bot, types):
+    bot.send_message(message_chat_id, 'Узнать информацию можно в личном кабинете SAP Портала: <b>Услуги и сервисы - '
+                                      'Корпоративные сервисы самообслуживания - Командировки: заявка и оформление</b>',
+                                      parse_mode='HTML', reply_markup= menu_button(types))
+
--- main.py
+++ main.py
@@ -8,61 +8,87 @@ commands = {  # command description used in the "help" command
     'start '       : 'Знакомство с ботом'
 }
 
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     bot.send_message(message.chat.id, "Привет, " + str(message.from_user.first_name)
                      + ", это твой личный HR друг! Я всегда готов помочь тебе с вопросами")
-    button1 = types.InlineKeyboardButton(text = "1. ☕", callback_data = "Coffee")
-    button2 = types.InlineKeyboardButton(text = "2. ❓", callback_data = "Personnel administration")
-    button3 = types.InlineKeyboardButton(text = "3. 🕵️‍♂️ ", callback_data = "Study")
-    button4 = types.InlineKeyboardButton(text = "4. 📈 ", callback_data = "Grow")
+    button1 = types.InlineKeyboardButton(text = "1. ☕", callback_data = "coffee")
+    button2 = types.InlineKeyboardButton(text = "2. ❓", callback_data = "personnel administration")
+    button3 = types.InlineKeyboardButton(text = "3. 🕵️‍♂️ ", callback_data = "study")
+    button4 = types.InlineKeyboardButton(text = "4. 📈 ", callback_data = "grow")
     button5 = types.InlineKeyboardButton(text = "5. 📴 ", callback_data = "IT")
-    button6 = types.InlineKeyboardButton(text = "6. 📛", callback_data = "Not found")
+    button6 = types.InlineKeyboardButton(text = "6. 📛", callback_data = "not found")
     markup = types.InlineKeyboardMarkup()
-    markup.add(button1,button2,button3,button4,button5,button6)
+    markup.add(button1, button2, button3, button4, button5, button6)
     bot.send_message(message.chat.id, "Чем тебе помочь?\n"
                                       "1. Хочу выпить кофе с коллегой ☕\n"
                                       "2. У меня вопрос по кадровому администрированию ❓\n"
                                       "3. Хочу учиться/развиваться 🕵️‍♂️ \n"
                                       "4. Хочу расти и/или перемещаться в компании 📈 \n"
                                       "5. У меня вопрос по административной или ИТ поддержке 📴 \n"
-                                      "6. Я не нашел ответа на свой вопрос 📛\n"
-                     , reply_markup=markup)
+                                      "6. Я не нашел ответа на свой вопрос 📛\n", reply_markup=markup)
+
+    bot_message = 5  # Переменная для удаления предыдущих сообщений
 
     @bot.callback_query_handler(func=lambda call: True)
     def callback_query(call):
-        if (call.data) == "Start":
+        if call.data == "start":
             send_welcome(message)
-        if (call.data) =="Coffee":
-            from functions import Coffee
-            Coffee(message.chat.id, bot, types)
 
-        if (call.data) =="Personnel administration":
-            from functions import Pers_adm
-            Pers_adm(message.chat.id, bot, types)
+        if call.data =="coffee":
+            from functions import coffee
+            coffee(message.chat.id, bot, types)
 
-        if (call.data) == "KS":
-            from functions import KS
-            KS(message.chat.id, bot, types)
+        if call.data =="personnel administration":
+            from functions import pers_adm
+            nonlocal bot_message
+            bot_message = pers_adm(message.chat.id, bot, types)
 
-        if (call.data) == "Study":
-            from functions import Study
-            Study(message.chat.id, bot, types)
+        if call.data == "KS":
+            bot.delete_message(bot_message.chat.id, bot_message.message_id)
+            from functions import KSorGPN_S
+            KSorGPN_S(message.chat.id, bot, types, False)
 
-        if (call.data) == "Perenos":
-            from functions import Perenos
-            Perenos(message.chat.id, bot, types)
+        if call.data == "study":
+            from functions import study
+            study(message.chat.id, bot, types)
 
-        if (call.data) == "Spravka":
-            from functions import Spravka
-            Spravka(message.chat.id, bot, types)
+        if call.data == "perenosKS":
+            from functions import perenosKS
+            perenosKS(message.chat.id, bot, types)
 
-        if (call.data) == "Bolnichnyi":
-            from functions import Bolnichnyi
-            Bolnichnyi(message.chat.id, bot, types)
+        if call.data == "spravkaKS":
+            from functions import spravkaKS
+            spravkaKS(message.chat.id, bot, types)
 
-        if (call.data) == "Komandirovka":
-            from functions import Komandirovka
-            Komandirovka(message.chat.id, bot, types)
+        if call.data == "bolnichnyiKS":
+            from functions import bolnichnyiKS
+            bolnichnyiKS(message.chat.id, bot, types)
+
+        if call.data == "komandirovkaKS":
+            from functions import komandirovkaKS
+            komandirovkaKS(message.chat.id, bot, types)
+
+        if call.data == "GPN-S":
+            bot.delete_message(bot_message.chat.id, bot_message.message_id)
+            from functions import KSorGPN_S
+            KSorGPN_S(message.chat.id, bot, types, True)
+
+        if call.data == "perenosGPN_S":
+            from functions import perenosGPN_S
+            perenosGPN_S(message.chat.id, bot, types)
+
+        if (call.data) == "spravkaGPN_S":
+            from functions import komandirovkaGPN_S
+            komandirovkaGPN_S(message.chat.id, bot, types)
+
+        if (call.data) == "bolnichnyiGPN_S":
+            from functions import komandirovkaGPN_S
+            komandirovkaGPN_S(message.chat.id, bot, types)
+
+        if (call.data) == "komandirovkaGPN_S":
+            from functions import komandirovkaGPN_S
+            komandirovkaGPN_S(message.chat.id, bot, types)
 
 bot.polling(none_stop=False, interval=0, timeout=20)
